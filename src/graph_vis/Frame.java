0f300ec61493a354843a8cd39c381393a68745f4
lenamiz <49653259+lenamiz@users.noreply.github.com>
2021-05-08 22:49:27 +0200

    add Frame class

---
+++ src/graph_vis/Frame.java
@@ -0,0 +1,22 @@
+package graph_vis;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Frame extends JFrame {
+    private static final int WIDTH = 400;
+    private static final int HEIGHT = 300;
+
+    public Frame(){
+        super("Graph visualisation");
+
+        JPanel dataPanel = new DataPanel(this);
+        add(dataPanel);
+
+        setPreferredSize(new Dimension(WIDTH,HEIGHT));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+}
